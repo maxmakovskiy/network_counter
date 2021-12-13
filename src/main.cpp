094883796fa9b021bade78b5a866e00aba3d4d83
maxmakovskiy <makovskiymax@gmail.com>
2021-12-13 23:58:30 +0200

    Basic example of using FTXUI

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,28 @@
 #include <iostream>
-#include <string>
-#include "manager.h"
-//#include <sqlite3.h>
-
-/*
-static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
-   int i;
-   for(i = 0; i<argc; i++) {
-      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-   }
-   printf("\n");
-   return 0;
-}
-*/
+//#include "manager.h"
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
 
 int main()
 {
+    using namespace ftxui;
 
+    Element doc = hbox({
+                text("Left") | border,
+                text("Middle") | border | flex,
+                text("right") | border
+            });
+   
+    auto screen = Screen::Create(Dimension::Full(),
+            Dimension::Fit(doc));
+    Render(screen, doc);
+    screen.Print();
+
+/*
     network_counter::SnifferManager manager;
 //    manager.AssignFile("log1.txt");
     manager.Process();
-
-/*
-    sqlite3 *db;
-    char *zErrMsg = 0;
-    int error_code;
-    std::string query = "CREATE TABLE COMPANY("  \
-      "ID INT PRIMARY KEY     NOT NULL," \
-      "NAME           TEXT    NOT NULL," \
-      "AGE            INT     NOT NULL," \
-      "ADDRESS        CHAR(50)," \
-      "SALARY         REAL );";
-
-    error_code = sqlite3_open("test.db", &db);
-    if(error_code) // SQLITE_OK == 0
-    {
-      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-      sqlite3_close(db);
-      return(1);
-    }
-
-    error_code = sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
-    if(error_code != SQLITE_OK)
-    {
-      fprintf(stderr, "SQL error: %s\n", zErrMsg);
-      sqlite3_free(zErrMsg);
-    }
-    sqlite3_close(db);
 */
-
 
     return 0;
 }
